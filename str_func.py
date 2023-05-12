f18da254e0a66aa0d6f96c538c6b453d69bb2c80
Tigran O. Saatchyan <117544809+tigran-saatchyan@users.noreply.github.com>
2023-05-12 14:14:40 +0400

    Update str_func.py
    adds Dockstrings to str_to_upper function

--- str_func.py
+++ str_func.py
@@ -1,2 +1,7 @@
 def str_to_upper(substr):
+    """
+    Alter text to uppercase
+    :param substr: - string to change
+    :return: - new string transformed to uppercase
+    """
     return substr.upper()
