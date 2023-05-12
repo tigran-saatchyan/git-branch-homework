0f3f7c178f546ebe65838fcdd8b0fb4d6d48a4e2
Tigran Saatchyan TSA <mr.saatchyan@yandex.com>
2023-05-12 14:19:20 +0400

    adds str_to_title function adds docstrings to all functions

--- str_func.py
+++ str_func.py
@@ -1,2 +1,20 @@
 def str_to_upper(substr):
+    """
+    Transforms test to 'upper' style text
+    :param substr: text to convert
+    :type substr: string
+    :return: converted text
+    :rtype: string
+    """
     return substr.upper()
+
+
+def str_to_title(substr):
+    """
+    Transforms test to 'Title' style text
+    :param substr: text to convert
+    :type substr: string
+    :return: converted text
+    :rtype: string
+    """
+    return substr.title()
